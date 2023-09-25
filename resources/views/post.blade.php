407d05ec38fa9501133ac14e33300560b0f35c49
stiliyan <stiliyan_2001@abv.bg>
2023-09-25 12:17:28 +0300

    making three static posts

---
+++ resources/views/post.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My blog</title>
+    <link rel="stylesheet" href="./css/app.css">
+</head>
+<body>
+
+
+    <article>
+
+        <h1>{{$post}}</h1>
+        <h1>My First Blog Post</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+            numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+            optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+           </p>
+
+    </article>
+
+
+    <p><a href="/">GO BACK</a></p>
+
+</body>
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My blog</title>
+    <link rel="stylesheet" href="./css/app.css">
+</head>
+<body>
+
+
+    <article>
+
+        <h1><a href="/post">My First Blog Post</a></h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+            numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+            optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+           </p>
+
+    </article>
+
+    <article>
+
+        <h1><a href="/post">My Second Blog Post</a></h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+            numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+            optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+            obcaecati tenetur iure eius earum ut molestias architecto voluptate aliqua
+            quia. Quo neque err </p>
+
+    </article>
+
+    <article>
+
+        <h1><a href="/post">My Third Blog Post</a></h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+            numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesen
+            tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+            quia. Quo neque error r</p>
+
+    </article>
+
+
+
+
+<script src="./js/app.js"></script>
+</body>
+</html>
